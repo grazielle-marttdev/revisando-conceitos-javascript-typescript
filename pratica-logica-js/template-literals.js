13d58f5fa55506b2848b1b607961a8dd021c26e8
Grazielle <gzassuncaomartins@gmail.com>
2026-06-10 10:41:44 -0300

    feat: feat: Adiciona exercício sobre template literals

---
+++ pratica-logica-js/template-literals.js
@@ -0,0 +1,7 @@
+const nome = "Grazielle";
+const idade = "20";
+const cidade = "Luziânia-GO";
+
+console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
+
+// Template literals (ou template strings) são uma forma de criar strings em JavaScript que permitem a interpolação de expressões e variáveis de forma mais fácil e legível. Elas são delimitadas por crases (`) em vez de aspas simples ou duplas. Dentro das template literals, você pode usar a sintaxe ${expressão} para incluir variáveis ou expressões JavaScript diretamente na string, sem a necessidade de concatenar com o operador +. Isso torna o código mais limpo e fácil de ler, especialmente quando se trata de strings complexas ou multilinhas.
